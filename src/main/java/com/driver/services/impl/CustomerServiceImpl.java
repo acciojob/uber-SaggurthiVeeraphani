cea40df64563ad132526f4333f77e09ab2ee6860
Veera Phani <veeraphani21@gmail.com>
2023-01-25 21:48:08 +0530

    irritating

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -89,7 +89,7 @@ public class CustomerServiceImpl implements CustomerService {
 		//Complete the trip having given trip Id and update TripBooking attributes accordingly
 		TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
 		tripBooking.setStatus(TripStatus.COMPLETED);
-		tripBooking.setBill(0);
+		//gitripBooking.setBill();
 
 		Driver driver = tripBooking.getDriver();
 		driver.getCab().setAvailable(true);
